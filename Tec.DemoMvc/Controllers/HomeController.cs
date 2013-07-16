dd5409593166adde73f2d756c585e381687f2de5
MichaelZhao <zhaowei54@gmail.com>
2013-07-16 11:16:40 +0800

    checkin for teamcity

--- Tec.DemoMvc/Controllers/HomeController.cs
+++ Tec.DemoMvc/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Tec.DemoMvc.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.TeamCity checking";
 
             return View();
         }
