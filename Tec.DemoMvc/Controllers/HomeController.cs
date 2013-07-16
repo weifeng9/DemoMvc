e6e5a7eb2ff5675ceafec0e41dc3c8e979c6a6c6
MichaelZhao <zhaowei54@gmail.com>
2013-07-16 11:18:45 +0800

    faild checkin

--- Tec.DemoMvc/Controllers/HomeController.cs
+++ Tec.DemoMvc/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace Tec.DemoMvc.Controllers
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.TeamCity checking";
 
-            return View();
+            //return View();
         }
 
         public ActionResult About()
